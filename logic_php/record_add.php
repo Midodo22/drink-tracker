d79fcc867f4f15b879861e7a9f43b2401de4260b
Tamilala_school <tamilala.cs12@nycu.edu.tw>
2025-12-11 22:14:16 +0800

    Just need to fill in the $_POST[""] s yeeeeet

--- logic_php/record_add.php
+++ logic_php/record_add.php
@@ -3,7 +3,10 @@
 require "db_connect.php";
 $stmt = $conn->stmt_init();
 
+$username = $_SESSION['username'];
+
 $brand_name = $_POST[""];
+// $brand_id = $_POST[""];
 $drink_name = $_POST[""];
 $toppings = $_POST[""];
 $temp = $_POST[""];
@@ -12,10 +15,23 @@ $sugar = $_POST[""];
 $calories = 0;
 $price = 0;
 
-// Get brand id (can delete if not necassary)
+// Get user id
+$sql = "SELECT users.id AS id
+		FROM users
+		WHERE users.username = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$user_id = $result->fetch_all(MYSQLI_ASSOC)[0]["id"];
+$stmt->close();
+
+// Get brand id (can delete if not needed)
 $sql = "SELECT brands.id AS id
-        FROM brands
-        WHERE brands.name = ?";
+		FROM brands
+		WHERE brands.name = ?";
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $brand_name);
@@ -27,11 +43,11 @@ $stmt->close();
 
 // Get drink price and caloires
 $sql = "SELECT drinks.price AS price, drinks.calories AS calories
-        FROM drinks
-        WHERE drinks.brand_id = ? AND drinks.name = ?";
+		FROM drinks
+		WHERE drinks.brand_id = ? AND drinks.name = ?";
 
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $brand_name);
+$stmt->bind_param("ss", $brand_id, $drink_name);
 $stmt->execute();
 
 $result = $stmt->get_result();
@@ -41,6 +57,43 @@ $price += $tmp["price"];
 $calories += $tmp["calories"];
 $stmt->close();
 
-// Get topping prive and calories
+// Get topping price and calories
+$sql = "SELECT toppings.price AS price, toppings.calories AS calories
+		FROM toppings
+		WHERE toppings.brand_id = ? AND toppings.name = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $brand_id, $drink_name);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$tmp = $result->fetch_all(mode: MYSQLI_ASSOC)[0];
+
+$price += $tmp["price"];
+$calories += $tmp["calories"];
+$stmt->close();
+
+// Get last record_id of user
+$sql = "SELECT MAX(records.record_id) AS id
+		FROM records
+		WHERE records.user_id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $user_id);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$record_id = $result->fetch_all(MYSQLI_ASSOC)[0]["id"];
+$record_id++;
+$stmt->close();
+
+// Insert into database
+$sql = "INSERT INTO records (user_id, record_id, brand_id, drink_name, toppings, temp, sugar, calories, price)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("n ", $user_id, $record_id, $brand_id, $drink_name, $toppings, $temp, $sugar, $calories, $price);
+
+$stmt->execute();
 
 ?>
